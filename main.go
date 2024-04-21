22a0dfca76efa1a2bb262bbe3cd97cbd5f5da5ee
Cathy Wrenhold <40322939+cwrenhold@users.noreply.github.com>
2024-04-21 15:29:11 +0100

    Minor refactoring to make things easier later

--- main.go
+++ main.go
@@ -11,6 +11,65 @@ import (
 	"github.com/joho/godotenv"
 )
 
+func ConvertToEnv(variables map[string]string) string {
+	output := ""
+
+	for key, values := range variables {
+		output += fmt.Sprintf("%s=%s\n", key, values)
+	}
+
+	return output
+}
+
+func ConvertToJson(variables map[string]string) string {
+	// Use a map[string]interface{} as a generic interface for the whole object
+	data := make(map[string]interface{})
+
+	for key, values := range variables {
+		// Assume any double underscore indicates a nested object (like in languages like C#)
+		parts := strings.Split(key, "__")
+
+		var findParent func(parent map[string]interface{}, parts []string) map[string]interface{}
+
+		// Recursive function to walk through the parts determining how deeply nested the key actually is
+		findParent = func(parent map[string]interface{}, parts []string) map[string]interface{} {
+			// If this is the last part, the current parent is the final parent for the key
+			if len(parts) <= 1 {
+				return parent
+			}
+
+			currentPart := parts[0]
+
+			// If this part doesn't already exist, created it as a new nested map[string]interface{}
+			val, exists := parent[currentPart]
+			if !exists {
+				parent[currentPart] = make(map[string]interface{})
+				val = parent[currentPart]
+			}
+
+			currentMap := val.(map[string]interface{})
+
+			return findParent(currentMap, parts[1:])
+		}
+
+		parent := findParent(data, parts)
+
+		// The last part is the part of the key which is needed in the last parent
+		// For example, NESTED__VALUE relates to the VALUE key within the NESTED object
+		lastPart := parts[len(parts)-1]
+		parent[lastPart] = values
+	}
+
+	// Use json.Marshal to convert this object to a json string to do some sanity checks
+	jsonStr, err := json.Marshal(data)
+
+	if err != nil {
+		log.Fatal("Unable to process json data")
+	}
+
+	return string(jsonStr)
+}
+
 func main() {
 	err := godotenv.Load()
 
@@ -52,61 +111,18 @@ func main() {
 	envHandler := func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "text/plain")
 
-		for key, values := range variables {
-			w.Write([]byte(fmt.Sprintf("%s=%s", key, values) + "\n"))
-		}
+		output := ConvertToEnv(variables)
+
+		w.Write([]byte(output))
 	}
 
 	// Serve up a json format
 	jsonHandler := func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "application/json")
 
-		// Use a map[string]interface{} as a generic interface for the whole object
-		data := make(map[string]interface{})
+		output := ConvertToJson(variables)
 
-		for key, values := range variables {
-			// Assume any double underscore indicates a nested object (like in languages like C#)
-			parts := strings.Split(key, "__")
-
-			var findParent func(parent map[string]interface{}, parts []string) map[string]interface{}
-
-			// Recursive function to walk through the parts determining how deeply nested the key actually is
-			findParent = func(parent map[string]interface{}, parts []string) map[string]interface{} {
-				// If this is the last part, the current parent is the final parent for the key
-				if len(parts) <= 1 {
-					return parent
-				}
-
-				currentPart := parts[0]
-
-				// If this part doesn't already exist, created it as a new nested map[string]interface{}
-				val, exists := parent[currentPart]
-				if !exists {
-					parent[currentPart] = make(map[string]interface{})
-					val = parent[currentPart]
-				}
-
-				currentMap := val.(map[string]interface{})
-
-				return findParent(currentMap, parts[1:])
-			}
-
-			parent := findParent(data, parts)
-
-			// The last part is the part of the key which is needed in the last parent
-			// For example, NESTED__VALUE relates to the VALUE key within the NESTED object
-			lastPart := parts[len(parts)-1]
-			parent[lastPart] = values
-		}
-
-		// Use json.Marshal to convert this object to a json string to do some sanity checks
-		jsonStr, err := json.Marshal(data)
-
-		if err != nil {
-			log.Fatal("Unable to process json data")
-		}
-
-		w.Write([]byte(jsonStr))
+		w.Write([]byte(output))
 	}
 
 	http.HandleFunc("/env", envHandler)
